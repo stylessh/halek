5117d2fad588ceedc211ce74d3bcd4cc13d41507
stylessh <stylesshjs@gmail.com>
2019-08-25 13:24:02 -0400

    Version 2 - Now the page is responsive

--- js/navbar.js
+++ js/navbar.js
@@ -21,8 +21,7 @@ window.addEventListener("scroll", () => {
 
 // dinamic navbar
 const dinamicNav = () => {
-  const sections =
-    document.querySelectorAll("section");
+  const sections = document.querySelectorAll("section");
   const navLinks = document.querySelectorAll(".navbar a");
 
   sections.forEach(sec => {
@@ -39,3 +38,53 @@ const dinamicNav = () => {
 };
 
 dinamicNav();
+
+// responsive navbar
+
+var media = window.matchMedia("(max-width: 901px)")
+  
+    if (media.matches) {
+      var t1 = new TimelineMax({ paused: true });
+
+      t1.to(".toggle-btn .one", 0.8, {
+        y: 6,
+        rotation: 45,
+        ease: Expo.easeInOut
+      });
+
+      t1.to(".toggle-btn .two", 0.8, {
+        y: -6,
+        rotation: -45,
+        ease: Expo.easeInOut,
+        delay: -0.8
+      });
+
+      t1.to(".navbar", 1, {
+        top: "0%",
+        ease: Expo.easeInOut,
+        delay: -1
+      });
+
+      t1.staggerFrom(
+        ".navbar li",
+        1,
+        { x: -200, opacity: 0, ease: Expo.easeOut },
+        0.3
+      );
+      t1.reverse();
+
+    const navLinks = document.querySelectorAll(".navbar a");
+    const toggleBtn = document.querySelector(".toggle-btn");
+
+    toggleBtn.addEventListener("click", () => {
+      t1.reversed(!t1.reversed());
+    });
+
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        t1.reversed(!t1.reversed());
+      });
+    });
+
+  }
+
